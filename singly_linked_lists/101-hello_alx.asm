c6d3d851dac26a5b7c79ca76b1e7b0363c8eaa72
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-05-10 07:36:28 +0100

    Fix assembly output to Hello, Holberton

--- singly_linked_lists/101-hello_alx.asm
+++ singly_linked_lists/101-hello_alx.asm
@@ -1,18 +1,18 @@
-; 64-bit NASM assembly: prints "Hello, ALX\n" using printf
+; 64-bit NASM assembly: prints "Hello, Holberton\n" using printf
 ; Compile: nasm -f elf64 101-hello_alx.asm && gcc -no-pie -std=gnu89 101-hello_alx.o -o hello
 
 section .data
-    msg db "Hello, ALX", 10, 0   ; 10 = newline, 0 = null terminator
+    msg db "Hello, Holberton", 10, 0   ; 10 = newline, 0 = null terminator
 
 section .text
     global main
     extern printf
 
 main:
-    push    rbp                 ; prologue (optional but nice)
+    push    rbp                 ; set up stack frame (optional but clean)
     mov     rdi, msg            ; first argument: pointer to the string
-    xor     eax, eax            ; 0 indicates no floating-point arguments
+    xor     eax, eax            ; number of floating-point arguments = 0
     call    printf              ; call printf(msg)
-    pop     rbp                 ; epilogue
+    pop     rbp                 ; restore stack frame
     xor     eax, eax            ; return 0
     ret
