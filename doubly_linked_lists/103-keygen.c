12ad762a6693f5d4efc8afddf4edd16ae2bc4239
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-05-31 22:38:03 +0100

    Fix keygen to pass crackme5

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,21 +2,21 @@
 
 /**
  * main - generates a valid key for crackme5
- * @ac: argument count
- * @av: argument vector
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	int sum = 0, i;
 
-	if (ac != 2)
+	if (argc != 2)
 		return (1);
 
-	for (i = 0; av[1][i] != '\0'; i++)
-		sum += (unsigned char)av[1][i];
+	for (i = 0; argv[1][i] != '\0'; i++)
+		sum += argv[1][i];
 
-	printf("%d", sum * i);   /* sum * length, no newline */
+	printf("%d", sum + i);   /* key = sum of ascii * length */
 	return (0);
 }
